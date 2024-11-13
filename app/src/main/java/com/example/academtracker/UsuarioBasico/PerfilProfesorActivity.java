817d4b71bfc31d8e52a83adbd448b16ebe469bec
Marcelo935 <marcelosanchez005@gmail.com>
2024-11-13 02:07:13 -0600

    NavigationDrawer implementation

--- app/src/main/java/com/example/academtracker/UsuarioBasico/PerfilProfesorActivity.java
+++ app/src/main/java/com/example/academtracker/UsuarioBasico/PerfilProfesorActivity.java
@@ -2,20 +2,30 @@ package com.example.academtracker.UsuarioBasico;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import com.example.academtracker.LectorNFCActivity;
+import com.example.academtracker.NFCAlumno1Activity;
+import com.example.academtracker.NFCAlumno2Activity;
 import com.example.academtracker.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
@@ -30,17 +40,17 @@ import com.journeyapps.barcodescanner.CaptureActivity;
 import java.util.Map;
 import java.util.Objects;
 
-public class PerfilProfesorActivity extends AppCompatActivity {
+public class PerfilProfesorActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     MaterialButton regresar;
-
     Button scanner, NFC;
-
     String useremail = "";
-
     TextView emailprofesor,nombreprofesor;
+    DrawerLayout navDrawer;
+    Toolbar toolbar; // Añadir Toolbar aquí
 
     private FirebaseFirestore db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +62,11 @@ public class PerfilProfesorActivity extends AppCompatActivity {
         emailprofesor = findViewById(R.id.emailprofesor);
         scanner = findViewById(R.id.QRbutton);
         NFC = findViewById(R.id.NFCbutton);
+        navDrawer = findViewById(R.id.navDrawer);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar); // Establece el Toolbar como ActionBar
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         scanner.setOnClickListener(view -> startQRScanner());
 
@@ -112,7 +127,25 @@ public class PerfilProfesorActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // Configuración del toggle para abrir/cerrar el Drawer con el icono en la barra de acción
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, navDrawer, R.string.open_drawer, R.string.close_drawer);
+        navDrawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Verifica si el ActionBar está disponible antes de manipularlo
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        // Habilitar el botón de la barra de navegación
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Configurar el listener del NavigationView
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
 
     private void startQRScanner() {
         IntentIntegrator integrator = new IntentIntegrator(this);//Instanciar una clase para crear un nuevo objeto hijo
@@ -151,4 +184,33 @@ public class PerfilProfesorActivity extends AppCompatActivity {
                 });
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            navDrawer.openDrawer(GravityCompat.START);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int ItemId = item.getItemId();
+        if(ItemId == R.id.nav_alfredo){
+            Intent intent = new Intent(PerfilProfesorActivity.this, LectorNFCActivity.class);
+            startActivity(intent);
+            finish();
+        }else if(ItemId == R.id.nav_miguel){
+            Intent intent = new Intent(PerfilProfesorActivity.this, NFCAlumno1Activity.class);
+            startActivity(intent);
+            finish();
+        }else if(ItemId == R.id.nav_marcelo){
+            Intent intent2 = new Intent(PerfilProfesorActivity.this, NFCAlumno2Activity.class);
+            startActivity(intent2);
+            finish();
+        }
+
+        navDrawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
